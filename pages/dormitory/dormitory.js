cb4b4ca0c4cd0585ef684cf3a04ae51e84529988
MoreGZ <849851597@qq.com>
2018-01-01 23:06:58 +0800

    finish html

--- pages/dormitory/dormitory.js
+++ pages/dormitory/dormitory.js
@@ -5,7 +5,88 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    building:{
+      north:[
+        {
+          builingname:"1",
+          bed:"2"
+        },
+        {
+          builingname: "2",
+          bed: "24"
+        },
+        {
+          builingname: "3",
+          bed: "26"
+        },
+        {
+          builingname: "4",
+          bed: "45"
+        },
+        {
+          builingname: "5",
+          bed: "2"
+        },
+        {
+          builingname: "6",
+          bed: "21"
+        },
+        {
+          builingname: "7",
+          bed: "3"
+        },
+
+      ],
+      south: [
+        {
+          builingname: "8",
+          bed: "2"
+        },
+        {
+          builingname: "9",
+          bed: "2"
+        },
+        {
+          builingname: "10",
+          bed: "2"
+        },
+        {
+          builingname: "11",
+          bed: "2"
+        },
+        {
+          builingname: "12",
+          bed: "2"
+        },
+        {
+          builingname: "13",
+          bed: "2"
+        }
+      ],
+      teacher: [
+        {
+          builingname: "1",
+          bed: "2"
+        },
+        {
+          builingname: "2",
+          bed: "2"
+        },
+        {
+          builingname: "3",
+          bed: "2"
+        },
+        {
+          builingname: "4",
+          bed: "2"
+        },
+      ]
+    },
+    areamap:{
+      "north":"北苑",
+      "south":"南苑",
+      "teacher":"师苑"
+    }
   },
 
   /**
