6348206d000d79613128c676a70c760603a5031f
MoreGZ <849851597@qq.com>
2018-01-06 23:27:09 +0800

    1

--- pages/student_detail/student_detail.js
+++ pages/student_detail/student_detail.js
@@ -5,7 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    detail:{
+      name: "莫广智",
+      sex: "男",
+      grade: "2015",
+      college: "信息科学与技术学院",
+      id: "20151002208",
+      major:"软件工程",
+      classname:"软件工程1501",
+      phonenumber: "15602233126",
+      nativeplace: "广东清远",
+      dormitory:"南苑13-339",
+      counselor: "谢石顺",
+      counselor_phonenumber: "13710042673",
+      dormitory_change_information:[],
+      firstconnecter:"xxx",
+      conneter_phone:"2334444",
+      counselor: "谢石顺",
+      counselor_id: "2015100223",
+      ispoor:false,
+      isInsurance:true,
+      Remarks:"",
+      change_records:""
+    }
   },
 
   /**
